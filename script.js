ea6cde7380b3fbec9848277bd28296ab1660fe37
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-08-12 21:44:36 -0300

    Funcionalidad del boton C agregada

--- script.js
+++ script.js
@@ -5,5 +5,10 @@ const botones = document.querySelectorAll('.btn');
 botones.forEach(boton => {
     boton.addEventListener('click', () => {
         const botonApretado = boton.textContent;
+
+        if(boton.id === "c"){
+            pantalla.textContent = "0";
+            return;
+        }
     });
 });
